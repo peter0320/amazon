3542d53f0100cc7139f7018f444d49884cb0b62c
peter0320 <peyinos45@gmail.com>
2021-12-08 23:30:32 -0500

    modificacionees__colores_slider

--- src/app/modulos/servicios/servicios-routing.module.ts
+++ src/app/modulos/servicios/servicios-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SessionGuard } from 'src/app/guards/session.guard';
 import { CreateComponent } from './create/create.component';
 import { EditComponent } from './edit/edit.component';
 import { GetComponent } from './get/get.component';
@@ -7,12 +8,15 @@ import { GetComponent } from './get/get.component';
 const routes: Routes = [{
   path: 'create',
   component: CreateComponent,
+  canActivate: [SessionGuard]
 },{
   path: "edit/:id",
   component: EditComponent,
+  canActivate: [SessionGuard]
 },{
   path: 'get',
   component: GetComponent,
+  canActivate: [SessionGuard]
 },{
   path: '',
   redirectTo: 'get'
